af0aafcb3b143ff448166dbbab3f0d6fdbd6c69c
Mark Drehmann <markdrehmann@gmail.com>
2019-12-10 21:23:37 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -5,11 +5,6 @@ describe "conversions" do
     it 'given zero, returns 0.0' do
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
-      if answer == 0.0
-        puts "Passed!"
-      else
-        puts "Test failed: got #{answer} instead"
-      end
     end
   end
 end
